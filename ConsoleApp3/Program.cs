876ae2d14d5be0f6a739e407b87d2d52c68e3c44
kakaopiraten <55191176+kakaopiraten@users.noreply.github.com>
2021-09-24 16:39:47 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int input = 0;
+            int.TryParse(Console.ReadLine(), out input);
+            Console.WriteLine("Heres your number:" + Factorial(input));
+
+
+        }
+        
+        static double Factorial(int number)
+        {
+
+            if(number==0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
